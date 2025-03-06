5a1f34c86473bf7fbd91d3224905f5bae86d55de
aa24afl (paul) <aa24afl@herts.ac.uk>
2025-03-06 17:35:34 +0000

    member class created

---
+++ BPC/src/Member.java
@@ -0,0 +1,24 @@
+public class Member {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String phone;
+
+    public Member(String firstName, String lastName){
+        this.id = 0;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    public Member(String firstName, String lastName, String address, String phone){
+        this.id = 0;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.phone = phone;
+    }
+
+}

83f6212bc7b8b3b2f3c3d357842ab00e68fb65fe
ayobami-paul <aa24afl@herts.ac.uk>
2025-04-03 12:57:47 +0100

    created clinic test class

---
+++ BPC/src/test/java/ClinicTest.java
@@ -0,0 +1,68 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClinicTest {
+
+    @org.junit.jupiter.api.Test
+    void addTherapist() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void removeTherapist() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addPatient() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void removePatient() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAllPatients() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAllTherapists() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getClinicCalendar() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getClinicCalendar1() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void searchByExpertise() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void searchByTherapist() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void bookAppointment() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void cancelAppointment() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void attendAppointment() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAttendedCount() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAllAppointments() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAppointmentReport() {
+    }
+}
